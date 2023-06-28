dc34d232d0ad31536b1ba26c514a0844cd349eec
0xshikhar <shikhar@iiitmanipur.ac.in>
2023-06-28 23:34:46 +0530

    completed tournament generator integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,41 @@
 import "../styles/globals.css";
-import { useRouter } from "next/router";
 import MainLayout from "../layout/mainLayout";
+import {
+  LivepeerConfig,
+  ThemeConfig,
+  createReactClient,
+} from "@livepeer/react";
+import React, { createContext, useMemo } from "react";
+import LivepeerClient from "../client";
+import { AptosClient } from "aptos";
 
+export const AptosContext = createContext(AptosClient);
+
+const theme = {
+  colors: {
+    accent: "rgb(0, 145, 255)",
+    containerBorderColor: "rgba(0, 145, 255, 0.9)",
+  },
+  fonts: {
+    display: "Inter",
+  },
+};
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
+  // create an aptos client using the devnet endpoint on app mount
+  const aptosClient = useMemo(
+    () => new AptosClient("https://fullnode.devnet.aptoslabs.com/v1"),
+    []
+  );
 
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <AptosContext.Provider value={aptosClient}>
+      <LivepeerConfig client={LivepeerClient} theme={theme}>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </LivepeerConfig>
+    </AptosContext.Provider>
   );
 }
 
